329d4113ecb9b6c5d4dee6f0c9eba3744cb979a3
ValKhv <31601335+ValKhv@users.noreply.github.com>
2018-04-24 16:37:35 -0400

    Add files via upload

---
+++ LIBS/BGXNode/build-alpha-6.bat
@@ -0,0 +1,7 @@
+set JAVA_HOME=C:\Java\jdk-6
+set PROJECT_HOME=D:\depot\External Projects\i9500\Implementation\Source\
+set i9500_PROJECTS_HOME=%PROJECT_HOME%
+set JBOSS_HOME=F:\JBOSS\jboss-4.2.0.CR1\
+set JIBX_LIB=D:\depot\MNS\External Projects\i9500\Implementation\Source\lib\
+
+ant alpha
